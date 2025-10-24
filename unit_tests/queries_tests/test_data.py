4aa51d00d35afdf0073ccd195a015f175e09d7aa
khaledk2 <khaledk2@hotmail.com>
2025-10-24 20:55:00 +0100

    test

--- unit_tests/queries_tests/test_data.py
+++ unit_tests/queries_tests/test_data.py
@@ -102,7 +102,7 @@ image_owner_group = {"image": [["gene symbol", "cep120", 702, 5]]}
 
 images_keys = ["cell line", "gene symbol"]
 
-images_value_parts = ["he11", "pr11"]
+images_value_parts = ["hel", "prot"]
 
 csv_test_data = [
     {"key": "organism", "value": "homo sapiens", "no_results": 15756},
