0c172e0b0fcd65e551fe018c3a2d5a4239cbb815
khaledk2 <khaledk2@hotmail.com>
2025-10-25 10:36:05 +0100

    test2

--- unit_tests/queries_tests/test_quries.py
+++ unit_tests/queries_tests/test_quries.py
@@ -395,8 +395,8 @@ class BasicTestCase(unittest.TestCase):
             #            break
             #self.assertTrue (is_it_in)
 
-            # assert all(item in resource_keys for item in test_fed_terms)
-            assert any(item in test_fed_terms for item in resource_keys)
+            assert all(item in resource_keys for item in test_fed_terms)
+            #assert any(item in test_fed_terms for item in resource_keys) ===>first
 
     def test_data_sources(self):
         """
